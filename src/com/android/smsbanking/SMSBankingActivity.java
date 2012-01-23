bb51b76f6aa02a815c97252ffb4eccdc3047234d
unknown <natalia.ivashuta@.LGE.NET>
2012-01-23 14:22:38 +0400

    Add Broadcast base

--- src/com/android/smsbanking/SMSBankingActivity.java
+++ src/com/android/smsbanking/SMSBankingActivity.java
@@ -3,11 +3,16 @@ package com.android.smsbanking;
 import android.app.Activity;
 import android.os.Bundle;
 
+
 public class SMSBankingActivity extends Activity {
+	
+	public String bankAddress = "5556";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
     }
 }
