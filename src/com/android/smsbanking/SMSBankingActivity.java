75c13d615f86f591b13ee91163f5edf2c34e8045
unknown <natalia.ivashuta@.LGE.NET>
2012-01-24 13:48:30 +0400

    Send bank number to SMSReceiver and chhose sms from bank

--- src/com/android/smsbanking/SMSBankingActivity.java
+++ src/com/android/smsbanking/SMSBankingActivity.java
@@ -1,16 +1,27 @@
 package com.android.smsbanking;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 
 public class SMSBankingActivity extends Activity {
 	
-    /** Called when the activity is first created. */
+	Context context;
+	
+	private static final String BANK_ADDRESS = "5556";
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        context = getApplicationContext();
+        
+        Intent intent = new Intent(SMSReceiver.BANK_ADDRESS_ACTION); 
+        intent.putExtra(SMSReceiver.TYPE, BANK_ADDRESS); 
+        sendBroadcast(intent); 
     }
 }
--- src/com/android/smsbanking/SMSReceiver.java
+++ src/com/android/smsbanking/SMSReceiver.java
@@ -6,14 +6,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
+
 
 public class SMSReceiver extends BroadcastReceiver {
 	
-	String bankAddress = "5556";
+	public static final String BANK_ADDRESS_ACTION = "com.android.smsbanking.BANK_ADDRESS";
+	public static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+	public static final String TYPE = "address"; 
+	private static final String BANK_ADDRESS = "5556";
+
+	static String bankAddress = null;
+	String currBankAddress = null;
 	 @Override
 	  public void onReceive(Context context, Intent intent) {
 		 	
+		 //Log.d("NATALIA!!!", bankAddress);
+		 if (BANK_ADDRESS_ACTION.equals(intent.getAction()))
+		 {
+			 bankAddress = intent.getStringExtra(TYPE); 
+			 //Log.d("NATALIA!!!",  bankAddress);
+		 }
+		 else if (SMS_RECEIVED.equals(intent.getAction()))
+		 {
+			//Log.d("NATALIA!!!",  bankAddress);
 		 	Bundle bundle = intent.getExtras();        
 	        SmsMessage[] msgs = null;
 			SmsMessage msgs1 = null;
@@ -31,7 +48,7 @@ public class SMSReceiver extends BroadcastReceiver {
 	                {
 			            for (int i=0; i<msgs.length; i++){
 				            msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-			                str += "SMS from " + msgs[i].getOriginatingAddress();                     
+			                str += "SMS is from bank " + msgs[i].getOriginatingAddress();                     
 			                str += " :";
 			                str += msgs[i].getMessageBody().toString();
 			                str += "\n";        
@@ -41,6 +58,7 @@ public class SMSReceiver extends BroadcastReceiver {
 			        	str += "SMS is not from bank";
 		        }		 
 		 Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+		 }
 	 }
 
 }
