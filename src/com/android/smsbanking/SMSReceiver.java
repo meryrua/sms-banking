473118cb6443829bd2b57c13652b76a29f111bd1
unknown <natalia.ivashuta@.LGE.NET>
2012-01-24 14:52:10 +0400

    Add notification about new SMS from bank

--- src/com/android/smsbanking/SMSReceiver.java
+++ src/com/android/smsbanking/SMSReceiver.java
@@ -1,6 +1,9 @@
 package com.android.smsbanking;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,9 +19,13 @@ public class SMSReceiver extends BroadcastReceiver {
 	public static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 	public static final String TYPE = "address"; 
 	private static final String BANK_ADDRESS = "5556";
-
+	
+	private static int NOTIFICATION_ID = 0;
+	
 	static String bankAddress = null;
 	String currBankAddress = null;
+	private Notification notification;
+	
 	 @Override
 	  public void onReceive(Context context, Intent intent) {
 		 	
@@ -35,7 +42,7 @@ public class SMSReceiver extends BroadcastReceiver {
 	        SmsMessage[] msgs = null;
 			SmsMessage msgs1 = null;
 	        String str1 = "";            
-	        String str = "";            
+	        String smsMessage = "";            
 	        if (bundle != null)
 	        {
 	            //---retrieve the SMS message received---
@@ -48,17 +55,35 @@ public class SMSReceiver extends BroadcastReceiver {
 	                {
 			            for (int i=0; i<msgs.length; i++){
 				            msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-			                str += "SMS is from bank " + msgs[i].getOriginatingAddress();                     
-			                str += " :";
-			                str += msgs[i].getMessageBody().toString();
-			                str += "\n";        
+			                smsMessage += "SMS is from bank " + msgs[i].getOriginatingAddress();                     
+			                smsMessage += " :";
+			                smsMessage += msgs[i].getMessageBody().toString();
+			                smsMessage += "\n";        
 			            }
+			            setSMSNotification(context, smsMessage.subSequence(0, (smsMessage.length() - 1)));  
 	                }
-			        else
-			        	str += "SMS is not from bank";
+			        //else
+			        	//str += "SMS is not from bank";
 		        }		 
-		 Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+		 //Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
 		 }
 	 }
-
+		 
+	private void setSMSNotification(Context context, CharSequence notiDetail) {
+		String ns = Context.NOTIFICATION_SERVICE;
+	    NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
+	            
+	    String smsNoti = "New SMS from bank";
+	    long when = System.currentTimeMillis();
+	    int icon = R.drawable.icon;
+	    notification = new Notification(icon, smsNoti, when);
+	            
+	    Intent notiIntent = new Intent(context, SMSBankingActivity.class);
+	    PendingIntent launchIntent = PendingIntent.getActivity(context, 0, notiIntent, 0);
+	    notification.setLatestEventInfo(context, smsNoti.subSequence(0, (smsNoti.length() - 1)) , notiDetail, launchIntent);
+	            
+	    NOTIFICATION_ID ++;
+	    mNotificationManager.notify(NOTIFICATION_ID, notification);
+			 
+	}
 }
